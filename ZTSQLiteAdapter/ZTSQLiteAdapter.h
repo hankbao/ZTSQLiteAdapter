81d033e0aef29ac153be0615d89fef0a9bfe2133
Hank Bao <hankbao84@gmail.com>
2015-04-15 16:07:37 +0800

    Initial Commit

---
+++ ZTSQLiteAdapter/ZTSQLiteAdapter.h
@@ -0,0 +1,19 @@
+//
+//  ZTSQLiteAdapter.h
+//  ZTSQLiteAdapter
+//
+//  Created by Hank Bao on 15/4/15.
+//  Copyright (c) 2015年 zTap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZTSQLiteAdapter.
+FOUNDATION_EXPORT double ZTSQLiteAdapterVersionNumber;
+
+//! Project version string for ZTSQLiteAdapter.
+FOUNDATION_EXPORT const unsigned char ZTSQLiteAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZTSQLiteAdapter/PublicHeader.h>
+
+
